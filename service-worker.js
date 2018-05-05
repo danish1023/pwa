b324bf2664a7289f0129fc0b7560597d25a46289
danish1023 <danish1023@gmail.com>
2018-05-05 17:35:34 +0530

    fixing

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,7 @@
-var cacheName = 'cache-v1';
-var files = [
-  './',
-  './index.html?launcher=true',
-  './launcher-icon-1x.png',
-  './launcher-icon-2x.png',
-  './launcher-icon-4x.png',
-  './manifest.json'
-];
-
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(files);
-    })
-  );
 });
 
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName && key !== dataCacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
 });
