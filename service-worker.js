443aef73a8e35b51c98c315c3c72768d9a83743c
danish1023 <danish1023@gmail.com>
2018-05-07 16:38:47 +0530

    fixing

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 var cacheName = 'PWA-Cache';
 var filesToCache = [
   './',
-  './index.html'
+  './index.html?launcher=true'
 ];
 
 self.addEventListener('install', function(e) {
@@ -18,6 +18,29 @@ self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', (event) => {
   console.log('[ServiceWorker] Fetch');
+
+  var request = event.request;
+
+  //Tell the browser to wait for newtwork request and respond with below
+  event.respondWith(
+    //If request is already in cache, return it
+    caches.match(request).then((response) => {
+      if (response) {
+        return response;
+      }
+      //if request is not cached or navigation preload response, add it to cache
+      return fetch(request).then((response) => {
+        var responseToCache = response.clone();
+        caches.open(cacheName).then((cache) => {
+            cache.put(request, responseToCache).catch((err) => {
+              console.warn(request.url + ': ' + err.message);
+            });
+          });
+
+        return response;
+      });
+    })
+  );
 });
