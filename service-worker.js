ba148ada70e80668c5129cc4e3fae50adcd1df70
danish1023 <danish1023@gmail.com>
2018-05-07 17:21:19 +0530

    fixing

--- service-worker.js
+++ service-worker.js
@@ -24,7 +24,7 @@ self.addEventListener('activate', function (event) {
 self.addEventListener('fetch', function (event) {
   console.log('[ServiceWorker] Fetch');
   event.respondWith(
-    caches.match(request).then((response) => {
+    caches.match(event.request).then((response) => {
       if (response) {
         return response;
       }
