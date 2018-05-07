f2085e848c46fc38fb4d06e13374ce979fbdbc24
danish1023 <danish1023@gmail.com>
2018-05-07 16:21:10 +0530

    fixing

--- service-worker.js
+++ service-worker.js
@@ -14,16 +14,6 @@ self.addEventListener('install', function(e) {
   );
 });
 
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    })
-  );
-});
-
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
 });
