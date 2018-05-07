a87dfbd9ed825eff61517ee45090f354088e71d4
danish1023 <danish1023@gmail.com>
2018-05-07 16:16:16 +0530

    fixing

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,27 @@
+var cacheName = 'PWA-Cache';
+var filesToCache = [
+  './',
+  './index.html'
+];
+
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
 self.addEventListener('activate', function(e) {
